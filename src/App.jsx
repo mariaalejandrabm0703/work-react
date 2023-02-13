d513203aff9eba1c7337febd171c6fd1f941af16
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2023-02-13 18:29:33 -0500

    Add description app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+/**
+ * Description
+ * aplicación donde pueda darle mis ultimo 3 libros leidos y me recomiende 1 libro leer basado en esos
+ * adicionarle al buscar calificación de gusto del (malo)1-5(excelente)
+ * mostrar titulo, autor, portada, resumen y comentario de algun otro autor
+ * poder compartirlo en twitter
+ * la cantidad de libros ingresados sea n-cantidad
+ */
 function App() {
 
   return (
