2f6b53d488f296ff3a7fc1e794b8d6244c422da1
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-02-14 16:04:34 -0500

    feature: add style in form

--- src/App.jsx
+++ src/App.jsx
@@ -22,22 +22,34 @@ function App() {
   };
 
   return (
-    <section className="bg-gray-100 p-4 rounded-lg shadow-md">
+    <section className="bg-gray-100 text-center w-full mx-auto py-12 px-4">
       <main>
-        <h1>Te recomiendo un libro</h1>
+        <h1 className="text-3xl font-extrabold text-black">
+          Te recomiendo un libro
+        </h1>
         <p>Regálame tu último libro leído</p>
-        <form onSubmit={handleBookRead}>
-          <input
-            type="text"
-            id="book"
-            name="book"
-            value={book}
-            onChange={handleChange}
-          ></input>
-          <button type="submit">Buscar</button>
-        </form>
+        <div className="lg:mt-0 lg:flex-shrink-0">
+          <form onSubmit={handleBookRead}>
+            <input
+              type="text"
+              id="book"
+              name="book"
+              value={book}
+              onChange={handleChange}
+            ></input>
+            <div className="mt-12 inline-flex rounded-md shadow">
+              <button
+                type="submit"
+                className="py-4 px-6 flex justify-center items-center bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+              >
+                Buscar
+              </button>
+            </div>
+          </form>
+        </div>
+
         <p>Regálame tu último libro leído</p>
-        <div className="">
+        <div className="text-center text-gray-500 text-lg">
           <p>{recommendedBook}</p>
         </div>
       </main>
