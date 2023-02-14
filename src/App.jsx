a1950456abb7b3032b62bdcea74ec1f543a17603
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-02-14 15:52:24 -0500

    feature: add tailwindcss and initial form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
 /**
  * Description
@@ -10,12 +9,40 @@ import './App.css'
  * la cantidad de libros ingresados sea n-cantidad
  */
 function App() {
+  const recommendedBook = "Deberías leer ...";
+  const [book, setBook] = useState("");
+
+  const handleChange = (e) => {
+    setBook(e.target.value);
+  };
+
+  const handleBookRead = (event) => {
+    event.preventDefault();
+    console.log(book);
+  };
 
   return (
-    <div>
-      hola nuevo proyecto a crear TDD
-    </div>
-  )
+    <section className="bg-gray-100 p-4 rounded-lg shadow-md">
+      <main>
+        <h1>Te recomiendo un libro</h1>
+        <p>Regálame tu último libro leído</p>
+        <form onSubmit={handleBookRead}>
+          <input
+            type="text"
+            id="book"
+            name="book"
+            value={book}
+            onChange={handleChange}
+          ></input>
+          <button type="submit">Buscar</button>
+        </form>
+        <p>Regálame tu último libro leído</p>
+        <div className="">
+          <p>{recommendedBook}</p>
+        </div>
+      </main>
+    </section>
+  );
 }
 
-export default App
+export default App;
